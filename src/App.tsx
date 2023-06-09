ab01dca368e3b25e64bbfb93c5e4d563db861c86
Lianne Turnbull <lianne.turnbull@gmail.com>
2023-06-09 10:58:44 +0100

    remove old library

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,6 @@ import {
     Heading,
     Flex,
 } from "@chakra-ui/react";
-import { Document, Page } from "react-pdf";
 import { ColorModeSwitcher } from "./ColorModeSwitcher";
 import theme from "./theme";
 import ControlPanel from "./control-panel/ControlPanel";
@@ -17,13 +16,6 @@ import { Worker, Viewer, PageLayout } from "@react-pdf-viewer/core";
 import "@react-pdf-viewer/core/lib/styles/index.css";
 // @ts-ignore
 import temppdf from "./indigestion-brewing.pdf";
-
-// import { pdfjs } from "react-pdf";
-
-// pdfjs.GlobalWorkerOptions.workerSrc = new URL(
-//     "pdfjs-dist/build/pdf.worker.min.js",
-//     import.meta.url,
-// ).toString();
 
 const pageLayout: PageLayout = {
     buildPageStyles: () => ({
@@ -40,11 +32,6 @@ const pageLayout: PageLayout = {
 
 export const App = () => {
     const [pdf, updatePdf] = useState<Uint8Array | null>(null);
-    // const [numPages, setNumPages] = useState<number | null>(null);
-    // const onDocumentLoadSuccess = ({ numPages }: { numPages: number }) => {
-    //     setNumPages(numPages);
-    // };
-    console.log("pdf =", pdf);
     return (
         <ChakraProvider theme={theme}>
             <Box textAlign="center" fontSize="xl">
@@ -103,23 +90,6 @@ export const App = () => {
                                         pageLayout={pageLayout}
                                     />
                                 </Worker>
-                                // <Document
-                                //     file={pdf}
-                                //     onLoadSuccess={onDocumentLoadSuccess}
-                                // >
-                                //     {Array.from(
-                                //         new Array(numPages),
-                                //         (el, index) => (
-                                //             <Page
-                                //                 scale={1.5}
-                                //                 key={`page_${index + 1}`}
-                                //                 pageNumber={index + 1}
-                                //                 renderTextLayer={false}
-                                //                 renderAnnotationLayer={false}
-                                //             />
-                                //         ),
-                                //     )}
-                                // </Document>
                             )}
                         </Flex>
                     </GridItem>
