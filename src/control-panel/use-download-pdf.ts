b6c6f03a9395d31ce2165be652c3c90905412ce6
Dave <d.garnar@uk.cdw.com>
2023-08-01 18:49:52 +0100

    pointing to live api

--- src/control-panel/use-download-pdf.ts
+++ src/control-panel/use-download-pdf.ts
@@ -2,51 +2,51 @@ import { Dispatch, SetStateAction, useCallback, useState } from "react";
 import { ScriptSubmission } from "../api-types";
 
 const downloadPdf = async (script: ScriptSubmission) => {
-    return await fetch("http://localhost:8080/script", {
-        method: "POST",
-        mode: "cors",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify(script),
-    });
+  return await fetch("http://api.clocktower.guru:8081/script", {
+    method: "POST",
+    mode: "cors",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(script),
+  });
 };
 
 const concatUintArrayArrays = (a: Uint8Array, b: Uint8Array) => {
-    const c = new Uint8Array(a.length + b.length);
-    c.set(a, 0);
-    c.set(b, a.length);
-    return c;
+  const c = new Uint8Array(a.length + b.length);
+  c.set(a, 0);
+  c.set(b, a.length);
+  return c;
 };
 
 const useDownloadPdf = (
-    updatePdf: Dispatch<SetStateAction<Uint8Array | null>>,
+  updatePdf: Dispatch<SetStateAction<Uint8Array | null>>
 ) => {
-    const [fetchingPdf, updateFetchingPdf] = useState(false);
-    const createPdf = useCallback(
-        async (script: ScriptSubmission) => {
-            updateFetchingPdf(true);
-            const data = await downloadPdf(script);
-            if (data.body) {
-                const reader = await data.body.getReader();
-                let pdf = new Uint8Array();
-                let processing = true;
-                while (processing) {
-                    const { done, value } = await reader.read();
-                    if (value) {
-                        pdf = concatUintArrayArrays(pdf, value);
-                    }
-                    if (done) {
-                        processing = false;
-                    }
-                }
-                updatePdf(pdf);
-            }
-            updateFetchingPdf(false);
-        },
-        [updateFetchingPdf, updatePdf],
-    );
-    return { createPdf, fetchingPdf };
+  const [fetchingPdf, updateFetchingPdf] = useState(false);
+  const createPdf = useCallback(
+    async (script: ScriptSubmission) => {
+      updateFetchingPdf(true);
+      const data = await downloadPdf(script);
+      if (data.body) {
+        const reader = await data.body.getReader();
+        let pdf = new Uint8Array();
+        let processing = true;
+        while (processing) {
+          const { done, value } = await reader.read();
+          if (value) {
+            pdf = concatUintArrayArrays(pdf, value);
+          }
+          if (done) {
+            processing = false;
+          }
+        }
+        updatePdf(pdf);
+      }
+      updateFetchingPdf(false);
+    },
+    [updateFetchingPdf, updatePdf]
+  );
+  return { createPdf, fetchingPdf };
 };
 
 export default useDownloadPdf;
